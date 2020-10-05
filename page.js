84c9e83823ed23421fba27a2fa18ecea1eb87fc7
Felipe Gorostiaga <feligorostiaga@gmail.com>
2020-10-05 19:17:55 -0300

    Added banner

--- page.js
+++ page.js
@@ -4,27 +4,36 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-           to highlight the button that controls the panel */
-        this.classList.toggle("active");
+    acc[i].addEventListener("click", clickAcc);
+}
 
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+function clickAcc(_this) {
+    /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+}
+
+function checkHash() {
+    loc = document.getElementById(window.location.hash.substr(1)+"Button")
+    if (loc !== null) {
+        clickAcc.call(loc)
+    }
 }
 
 /* For tabs:
    -=-=-=-=-=-=-=-=-=-= */
-function openCity(evt, cityName) {
+function showTab(evt, tabId) {
     // Declare all variables
     var i, tabcontent, tablinks;
-    _this = document.getElementById(cityName);
+    _this = document.getElementById(tabId);
     parentdiv = _this.parentElement;
 
     // Get all elements with class="tabcontent" and hide them
